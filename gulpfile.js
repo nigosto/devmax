f1bde9a4fd4bafb18aecc6477ca494f52f376b50
Georgi Atanasov <36049559+nigosto@users.noreply.github.com>
2021-01-30 11:24:26 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let gulp = require("gulp"),
+	autoprefixer = require("gulp-autoprefixer"),
+	exec = require("gulp-exec"),
+	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass'),
+	cp = require("child_process");
+
+gulp.task("scss", function() {
+	return gulp.src( '_assets/scss/**/*.scss' )
+		.pipe( sass().on('error', sass.logError) )
+		.pipe( autoprefixer() )
+		.pipe( gulp.dest( './docs/css/' ) )
+		.pipe( browserSync.stream({ match: '**/*.css' }) )
+	;
+});
+
+// Jekyll
+gulp.task("jekylldev", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
+});
+
+gulp.task("jekyllprod", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl /devmax"], { stdio: "inherit", shell: true });
+});
+
+gulp.task("watch", function() {
+
+	browserSync.init({
+		server: {
+            baseDir: "./docs/"
+		}
+	});
+
+	gulp.watch( '_assets/scss/**/*.scss', gulp.series('scss') );
+
+	gulp.watch(
+		[
+			"./*.html",
+			"./_includes/*.html",
+			"./_layouts/*.html",
+			"./_posts/**/*.*"
+		]
+	).on('change', gulp.series('jekylldev', 'scss') );
+
+	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
+	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
+
+});
+
+gulp.task("default", gulp.series('jekylldev', 'scss', 'watch'));
+gulp.task("deploy", gulp.series('jekyllprod', 'scss'));
