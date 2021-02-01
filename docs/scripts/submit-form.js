b70930020b4a420c71f6753949c573db73d15932
Georgi Atanasov <36049559+nigosto@users.noreply.github.com>
2021-02-01 13:16:04 +0200

    Added recaptcha

--- docs/scripts/submit-form.js
+++ docs/scripts/submit-form.js
@@ -1,9 +1,13 @@
-window.onload = function() {
+window.onload = function () {
     let form = document.getElementsByClassName("site-contacts-message-form")[0];
-    
-    form.addEventListener("submit", function(e) {
+
+    form.addEventListener("submit", function (e) {
         e.preventDefault()
-        alert("success")
+        grecaptcha.ready(function () {
+            grecaptcha.execute('6Le_QUQaAAAAAH4O2mg_Cw35NYunY-xTlDeMvy6a', { action: 'submit' }).then(function (token) {
+                alert('success')
+            });
+        })
     })
 }
 
